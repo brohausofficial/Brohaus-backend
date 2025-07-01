88d3cae3883b3effea2c6da5011c5409b7a2d9aa
Jay Firke <firke0419@gmail.com>
2025-07-01 13:36:29 +0530

    Update `.idea/workspace.xml` and modify `email` field in `userModel` schema to no longer enforce uniqueness.

--- models/userModel.js
+++ models/userModel.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
     name: { type: String, required: false },
-    email: { type: String, required: false, unique: true },
+    email: { type: String, required: false, unique: false },
     mobileNumber: { type: String, required: true, unique: true },
     role: { type: String, required: true, enum: ['user', 'admin'] },
     isVerified: { type: Boolean, default: false },
