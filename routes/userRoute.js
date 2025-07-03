65ee0b4d31812f5663d5bfc37971f1a4eb763191
Jay Firke <firke0419@gmail.com>
2025-07-03 12:48:00 +0530

    Add `sendOTPAdmin` endpoint and update `sendOTP` logic in `userController` for OTP handling

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import {resendOTP, sendOTP, verifyOTP} from "../controllers/userController.js";
+import {resendOTP, sendOTP, sendOTPAdmin, verifyOTP} from "../controllers/userController.js";
 
 const userRouter = express.Router();
 
 userRouter.post('/sendOTP', sendOTP)
+userRouter.post('/sendOTPAdmin', sendOTPAdmin)
 userRouter.post('/verifyOTP',verifyOTP)
 userRouter.post('/resendOTP',resendOTP)
 
